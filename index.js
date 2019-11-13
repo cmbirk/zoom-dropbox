459314c4a24e992a8ba25d8d4d9be092e4a91fa6
Chris Birk <cmbirk@gmail.com>
2019-11-12 20:56:51 -0600

    update handlers / dependencies for first run

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-'use strict';
+const { PubSub } = require('@google-cloud/pubsub')
 
-exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
+const pubsub = new PubSub()
+
+exports.http = async (req, res) => {
+  return res.status(200).send('Hello World')
+}
+
+exports.getZoomVideos = async (event, context) => {
+  console.log("Getting Zoom videos")
+
+
+  const videos = [{ name: 'video1'}, { name: 'video2' }]
+
+  try {
+    const topic = pubsub.topic('get-zoom-video-success')
+
+    await topic.publish(Buffer.from(JSON.stringify(videos), 'utf8'))
+
+    console.log("Topic Published Successfully")
+  } catch (err) {
+    console.error(err)
+    throw(err)
+  }
 };
 
-exports.event = (event, callback) => {
-  callback();
+exports.saveToDropbox = async (event, callback) => {
+  console.log(event)
 };
