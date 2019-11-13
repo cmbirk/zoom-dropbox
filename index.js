0fd1c09565edeb60f52af58c2818370e6ca3d0e2
Chris Birk <cmbirk@gmail.com>
2019-11-12 21:01:02 -0600

    add linting

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ const { PubSub } = require('@google-cloud/pubsub')
 
 const pubsub = new PubSub()
 
-exports.http = async (req, res) => {
-  return res.status(200).send('Hello World')
-}
+exports.http = async (req, res) => res.status(200).send('Hello World')
 
 exports.getZoomVideos = async (event, context) => {
-  console.log("Getting Zoom videos")
+  console.log('Getting Zoom videos')
 
 
-  const videos = [{ name: 'video1'}, { name: 'video2' }]
+  const videos = [{ name: 'video1' }, { name: 'video2' }]
 
   try {
     const topic = pubsub.topic('get-zoom-video-success')
 
     await topic.publish(Buffer.from(JSON.stringify(videos), 'utf8'))
 
-    console.log("Topic Published Successfully")
+    console.log('Topic Published Successfully')
   } catch (err) {
     console.error(err)
-    throw(err)
+    throw (err)
   }
 };
 
